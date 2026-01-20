d4cdd3c9a5635eb8c8beb99dd706f476c29f0972
Bhavesh Kumar <kumarbhavesh771@gmail.com>
2026-01-20 15:17:35 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'http://127.0.0.1:3001',
-      process.env.FRONTEND_URL
-    ].filter(Boolean),
+    origin: true,
     credentials: true,
   });
   
